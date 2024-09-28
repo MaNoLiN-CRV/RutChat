a2fa314b2797546464c7fc72317b22824fad9ca2
MaNoLiN-CRV <manoliin.27@gmail.com>
2024-09-28 15:19:00 +0200

    User connection callouts

--- src/server.ts
+++ src/server.ts
@@ -16,8 +16,16 @@ const io = new Server(httpServer);
 // We serve the statics files in the "public" folder
 app.use(express.static('public'));
 
+/////// GLOBAL VARIABLES ////////
+
+const maxUsernameLength:number = 20;
+const maxMessageLength:number = 200;
+const defaultUsername = "Anonymous";
+const defaultWelcome:string = "Server: Welcome to RutChat by MaNoLiN !!! v1.0";
+
+///////////////////////////////////
 function welcome():string{
-    return "Server: Welcome to RutChat by MaNoLiN !!! "
+    return defaultWelcome;
 }
 
 let currentUsers:number;
@@ -31,25 +39,26 @@ io.on('connection', (socket) => {
     socket.on('username', (username:string) => {
 
         let finalUsername:string;
-        if (username.length < 15){
-            finalUsername = username
+        if (username.length < maxUsernameLength){
+            finalUsername = username;
         }
         else {
-            finalUsername = "Anonymous"
+            finalUsername = defaultUsername;
         }
         currentClient = new client(socket.id, finalUsername);
+        io.emit('message', currentClient.getUsername() + " has connected.")
+        io.emit('clients', "Users conected: " + currentUsers);
     });
 
     currentUsers += 1;
     socket.send(welcome())
-    socket.emit('message', currentClient.getUsername + " has connected.")
-    socket.emit('clients', "Users conected: " + currentUsers);
+    
 
     // Message event handling
     socket.on('message', (message: string) => {
         // We send the message to the clients.
         let filteredMessage:string = "[" + currentClient.getUsername() + "]" + " -> " + message;
-        if (!(filteredMessage.length > 250)){
+        if (!(message.length > maxMessageLength)){
             io.emit('message', filteredMessage);
         }
 
@@ -57,10 +66,10 @@ io.on('connection', (socket) => {
 
     // Client disconnection
     socket.on('disconnect', () => {
-        socket.emit('message', currentClient.getUsername + " has disconnected.")
-        console.log('Client disconnected: ', currentClient.getId());
-        currentUsers =- 1;
-        socket.emit('clients', "Users conected: " + currentUsers);
+        io.emit('message', currentClient.getUsername() + " has disconnected.")
+        console.log('Client disconnected: ', currentClient.getUsername());
+        currentUsers -= 1;
+        io.emit('clients', "Users conected: " + currentUsers);
     }); 
 });
 
