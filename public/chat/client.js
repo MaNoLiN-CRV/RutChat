873fa528518b94f171b0a71bce2faf12e5370d62
MaNoLiN-CRV <manoliin.27@gmail.com>
2024-09-29 09:28:09 +0200

    Console log removed

---
+++ public/chat/client.js
@@ -0,0 +1,63 @@
+const socket = io(); 
+
+const messagesDiv = document.getElementById('messages');
+const messageInput = document.getElementById('messageInput');
+const sendButton = document.getElementById('sendButton');
+const clientsConnected = document.getElementById('clientsConnected');
+
+const maxMessages = 100;
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    let username = '-';
+
+    while (username.trim() === '' || username === '-') {
+        username = prompt('Username: ');
+    }
+
+    socket.emit('username', username);
+});
+
+
+// Enviar mensaje al servidor
+function sendMessage(){
+    const message = messageInput.value; 
+    if (message) {
+        socket.emit('message', message); 
+        messageInput.value = ''; 
+    }
+}
+
+
+sendButton.addEventListener('click', () => {
+    const message = messageInput.value; 
+    if (message) {
+        socket.emit('message', message); 
+        messageInput.value = ''; 
+    }
+});
+
+
+socket.on('message', (message) => {
+    let messages = messagesDiv.childNodes;
+    if (messages.length > maxMessages){
+        messagesDiv.innerHTML = ''
+    }
+    const newMessage = document.createElement('div'); 
+    newMessage.textContent = message; 
+    messagesDiv.appendChild(newMessage); 
+    messagesDiv.scrollTop = messagesDiv.scrollHeight;
+    
+});
+
+socket.on('clients', (currentClients) => {
+    clientsConnected.innerHTML = currentClients
+});
+
+// También enviar mensaje al presionar Enter
+document.getElementById('messageInput').addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+        document.getElementById('sendButton').click();
+    }
+});
+
