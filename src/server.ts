3ce8a2da19cfed03577579e9d8b21f55c2f89d74
MaNoLiN-CRV <manoliin.27@gmail.com>
2024-10-19 19:32:55 +0200

    Code refactoring , https implementation, documentation

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,13 @@
 import express from "express";
-import { createServer } from "http";
-import { Server as httpServer } from "http";
 import { Server, Socket } from "socket.io";
-import { SalaConfig }  from "./SalaConfig.ts";
+import { SalaConfig } from "./SalaConfig.ts";
 import { CustomStorage } from "./CustomStorage.ts";
 import { CustomFileTransfer } from "./CustomFileTransfer.ts";
 import { events } from "./events/events.ts";
 import * as console from "console";
 const https = require('https');
-// FRAMEWORK CONFIG
-
-// Express is a framework that allows to easy control the endpoints of a http server
-// Also it can serve static resources like html , css and javascript
-// It uses the native http module from node.js (Blosted)
-
 export class server {
-  
+
   private _app: express.Application;
   private _httpsServer = https;
   private _io: Server;
@@ -23,10 +15,13 @@ export class server {
   private _storage: CustomStorage;
   private _customFileTransfer: CustomFileTransfer;
   private _eventsManager: events;
-  private _ID:number;
+  private _ID: number;
 
-
-  constructor(id:number) {
+  /**
+   * Server constructor
+   * @param id The folder id number (ex: .public1)
+   */
+  constructor(id: number) {
     this._ID = id;
     // The http server delegates the http requets to our Express framework.
     this._app = express();
@@ -41,22 +36,21 @@ export class server {
       .setDefaultUsername("Anonymous")
       .setSecretPass("secret");
 
-      this._storage = new CustomStorage(this._ID);
-      // We serve the statics files in the "public" folder
-      this._app.use(express.static("./client"));
-      this._app.use(express.static(this._storage.getPublicFolder()));
-      
-      this._customFileTransfer = new CustomFileTransfer(this._config, this._app, this._io,this._storage);
+    this._storage = new CustomStorage(this._ID);
+    // We serve the statics files in the "public" folder
+    this._app.use(express.static("./client"));
+    this._app.use(express.static(this._storage.getPublicFolder()));
 
-      // THIS ALWAYS HAS TO BE AT THE FINAL BLOSTE
-      this._eventsManager = new events(this._io, this._config,this._storage);
+    this._customFileTransfer = new CustomFileTransfer(this._config, this._app, this._io, this._storage);
+
+    // THIS ALWAYS HAS TO BE AT THE FINAL BLOSTE
+    this._eventsManager = new events(this._io, this._config, this._storage);
   };
 
   public start(port: number) {
-     this._httpsServer.createServer(this._config.certificates, this.app).listen(port, () => {
+    this._httpsServer.createServer(this._config.certificates, this.app).listen(port, () => {
       console.log("Server listening in https://localhost:" + port);
     });
-
   }
 
   // GETTERS AND SETTERS
@@ -65,9 +59,6 @@ export class server {
     return this._app;
   }
 
-  get httpServer(): httpServer {
-    return this._httpsServer;
-  }
 
   get io(): Server {
     return this._io;
