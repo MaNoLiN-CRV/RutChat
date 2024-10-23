a9e0caaa0ae91716b9a1843c572948aa3b57de3a
felix-caba <felixcaba@proton.me>
2024-10-23 13:06:24 +0200

    Second Initial Config. Added new Screens and Components.

--- rutchat-server/src/index.ts
+++ rutchat-server/src/index.ts
@@ -1,4 +1,21 @@
 import express, { Express, Request, Response } from 'express';
-import { Socket } from 'socket.io';
+
+import { Server as SocketIOSv, Socket } from 'socket.io';
+import { Server } from "http";
+
+// PRUEBA, MODIFICA TU EL PUTO BLOSTE
+
+const app: Express = express();
+
+const httpServer: Server = new Server(app);
+
+const io = new SocketIOSv(httpServer);
+
+app.use(express.json());
+
+const port: number = 3000;
+
+app.use(express.static('public'));
+
 
 
